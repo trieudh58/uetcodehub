ad37e262097a05beddfc0e2ed81da1374d843cd7
trieudh <trieudh@icts.vn>
2016-05-12 11:12:12 +0700

    rollback

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'first_name', 'last_name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+    public function problems()
+    {
+        return $this->hasMany('App\Problem');
+    }
+}
