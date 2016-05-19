2a7ec6cd9d07a2d04f4b09509ce9fe4398f9cd68
tamhoang1412 <tamhoang1412@gmail.com>
2016-05-19 17:21:15 +0700

    modify exam view

--- resources/views/exam/showExamCourses.blade.php
+++ resources/views/exam/showExamCourses.blade.php
@@ -1,5 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-    {{$exams}}
+    <div class="container">
+        <h2>
+            Kì thi
+        </h2>
+        <table class="table-display">
+            <thead>
+            <tr>
+                <td>Tên kì thi</td>
+                <td>Thời gian bắt đầu</td>
+                <td>Thời gian kết thúc</td>
+                <td>Thời gian làm bài</td>
+                <td>Trạng thái</td>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($exams as $exam)
+                <tr>
+                    <td>{{$exam->exam_name}}</td>
+                    <td>{{$exam->available_from}}</td>
+                    <td>{{$exam->available_to}}</td>
+                    <td>{{$exam->duration}} phút</td>
+                    <td>
+                        @if (!$exam->completed)
+                            <a data-toggle="modal" data-target="#enroll-modal">Bắt đầu thi</a>
+                            <div class="modal fade" id="enroll-modal" role="dialog">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h4 class="modal-title">Xác nhận tham gia</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <p>Bạn muốn bắt đầu kì thi {{$exam->exam_name}}?</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="location.href='{{url('/exams/'.$exam->exam_id)}}';">Đồng ý</button>
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Hủy bỏ</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @else
+                            <p>Đã hoàn thành</p>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/exam/showExamDetail.blade.php
+++ resources/views/exam/showExamDetail.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    {{$problems}}
+    <div class="container">
+        <table class="table-display">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Đề bài</th>
+                <th>Điểm tối đa</th>
+                <th>Thời gian chạy tối đa</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($problems as $index=>$problem)
+                <tr>
+                    <td><a>Bài {{$index + 1}}</a></td>
+                    <td>{{substr($problem->content, 0, 100)}}... </td>
+                    <td>{{$problem->pivot->score_in_exam}} </td>
+                    <td>{{$problem->time_limit}} s</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
